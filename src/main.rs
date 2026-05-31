5ac3bae74f95fb09c3b195e285fb435257f30c8c
ukaushik-98 <kaushikuk101@gmail.com>
2026-05-31 11:32:57 -0700

    start axum scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use axum::{Router, http::StatusCode, routing::any};
+use tokio::net::TcpListener;
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
     println!("Hello, world!");
+    let router = Router::new().route("/ws", any(ws_handler));
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, router).await
+}
+
+async fn ws_handler() -> StatusCode {
+    StatusCode::OK
 }
